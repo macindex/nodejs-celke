a33ef3304af1d5aff5f614ac1d26e7f552a3f801
marcio lira <machollanda@gmail.com>
2021-02-13 10:07:05 -0300

    express install and routs - #4

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-var http = require('http');
+// var http = require('http');
+const express = require("express")
 
-http.createServer(function(req, res){
-res.end("Gerenciador!");
-}).listen(8080);
+const app = express();
+
+app.get("/", function(req, res){
+
+    res.send("gerenciador financeiro");
+});
+app.get("/sobre-empresa", function(req, res){
+
+    res.send("Página sobre empresa");
+});
+
+
+app.get("/blog", function(req, res){
+
+    res.send("Página do blog");
+});
+
+app.get("/contato", function(req, res){
+
+    res.send("Página de contato");
+});
+
+//localhost:8080
+app.listen(8080);
+
+// http.createServer(function(req, res){
+// res.end("Gerenciador!");
+// }).listen(8080);
 
