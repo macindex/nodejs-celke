32b45c2018cb22f5d271ea1559c92386787e1d3c
marcio lira <machollanda@gmail.com>
2021-02-16 00:32:03 -0300

    register with node in db

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ var connection = mysql.createConnection({
     password : '123456',
     database : 'celke'
   });
+
+
+  connection.connect(function(err){
+    if (err) console.error('Erro ao realizar a conexão com BD: ' + err.stack); return;
+  });
+
+  connection.query("INSERT INTO users(nome, email) VALUES ('jessica', 'jessica@gmail.com')", function(err, result){
+    if(!err){
+    console.log('Usuario cadastrado com sucesso!');
+    }else{		
+    console.log('Usuario cadastrado com sucesso!');	
+    }
+  });
   
   connection.connect(function(err) {
     if (err) {
