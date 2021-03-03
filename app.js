36f8eb494daaeb9a317d00067fe360e79dffc1b6
marcio lira <machollanda@gmail.com>
2021-03-02 23:31:54 -0300

    creating api

--- app.js
+++ app.js
@@ -1,63 +1,79 @@
 const express = require("express");
 const app = express();
-const handlebars = require("express-handlebars");
-const bodyParser = require('body-parser');
-const moment = require('moment');
-const Pagamento = require("./models/Pagamento");
 
-app.engine('handlebars', handlebars({
-  defaultLayout: 'main',
-  helpers: {
-      formatDate: (date) => {
-          return moment(date).format('DD/MM/YYYY')
-      }
-  }
-}))
-app.set('view engine', 'handlebars')
+app.use(express.json());
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
+app.get("/", (req, res)=>{
 
-// app.get("/", function(req, res){
+});
 
-//   res.sendFile(__dirname + "/views/layouts/main.html");
-//   res.send("Guia para pagamentos");
+app.listen(8080, ()=>{
+  console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
+  
+})
+
+//_________________________________________
+// AULA 15
+// const express = require("express");
+// const app = express();
+// const handlebars = require("express-handlebars");
+// const bodyParser = require('body-parser');
+// const moment = require('moment');
+// const Pagamento = require("./models/Pagamento");
+
+// app.engine('handlebars', handlebars({
+//   defaultLayout: 'main',
+//   helpers: {
+//       formatDate: (date) => {
+//           return moment(date).format('DD/MM/YYYY')
+//       }
+//   }
+// }))
+// app.set('view engine', 'handlebars')
+
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+// // parse application/json
+// app.use(bodyParser.json())
+
+// // app.get("/", function(req, res){
+
+// //   res.sendFile(__dirname + "/views/layouts/main.html");
+// //   res.send("Guia para pagamentos");
+// // });
+// app.get("/pagamento", function(req, res){
+//   Pagamento.findAll({order: [['id', 'ASC' ]]}).then(function(pagamentos){
+//     res.render('pagamento', {pagamentos: pagamentos});
+//   })
+//   // res.sendFile(__dirname + "/views/pagamento.handlebars");
 // });
-app.get("/pagamento", function(req, res){
-  Pagamento.findAll({order: [['id', 'ASC' ]]}).then(function(pagamentos){
-    res.render('pagamento', {pagamentos: pagamentos});
-  })
-  // res.sendFile(__dirname + "/views/pagamento.handlebars");
-});
-app.get("/cad-pagamento", function(req, res){
-   res.render('cad-pagamento');
-});
+// app.get("/cad-pagamento", function(req, res){
+//    res.render('cad-pagamento');
+// });
 
-app.post("/add-pagamento", function(req, res){
-  Pagamento.create({
-    nome: req.body.nome,
-    valor: req.body.valor
-  }).then(function(){
-    res.redirect('/pagamento')
-    // res.send("Pagamento cadastrado com sucesso")
-  }).catch(function(erro){
-    res.send("Erro: Pagamento não foi cadastrado com sucesso!" + erro)
-  })
-  // res.send("Nome: " + req.body.nome + "<br>Valor: " + req.body.valor + "<br>");
-})
+// app.post("/add-pagamento", function(req, res){
+//   Pagamento.create({
+//     nome: req.body.nome,
+//     valor: req.body.valor
+//   }).then(function(){
+//     res.redirect('/pagamento')
+//     // res.send("Pagamento cadastrado com sucesso")
+//   }).catch(function(erro){
+//     res.send("Erro: Pagamento não foi cadastrado com sucesso!" + erro)
+//   })
+//   // res.send("Nome: " + req.body.nome + "<br>Valor: " + req.body.valor + "<br>");
+// })
 
-app.get('del-pagamento/:id', function(req, res){
-      Pagamento.destroy({
-        where: {id: req.params.id} 
-      }).then(function(){
-        res.redirect('/pagamento')
-          // res.send("Registro de pagamento deletado com sucesso!");
-      }).catch(function(erro){
-        res.send("Registro de pagamento não deletado!" + erro);
-      })
-})
+// app.get('del-pagamento/:id', function(req, res){
+//       Pagamento.destroy({
+//         where: {id: req.params.id} 
+//       }).then(function(){
+//         res.redirect('/pagamento')
+//           // res.send("Registro de pagamento deletado com sucesso!");
+//       }).catch(function(erro){
+//         res.send("Registro de pagamento não deletado!" + erro);
+//       })
+// })
 
 
 // _____________________________________________
